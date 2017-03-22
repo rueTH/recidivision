d3352ce5efe923046b65ab9e0f12992ed25a044e
Rebecca Wilkerson <nashvillesoftwareschool@Nashvilles-MacBook-Pro.local>
2017-03-22 14:07:38 -0500

    fixed add function issue

--- app/controllers/NewSearchCtrl.js
+++ app/controllers/NewSearchCtrl.js
@@ -5,32 +5,45 @@ app.controller("NewSearchCtrl", function($scope, $window, $location) {
   // this.selectedDropdownItem = null;
   // this.dropdownItems = ['dropdown 1', 'dropdown 2', 'dropdown 3'];
 //adding new search criteria:
-  $scope.parameters = [];
+
+
+  let searchPairs = [];
+  
+
+
+  $scope.searchPairs = searchPairs;
+  $scope.searchPair = {};
+  // $scope.searchPair = 
   $scope.parameter = '';
-
-  $scope.terms = [];
   $scope.newterm = '';  
-
+  
   $scope.saveButtonLabel = 'Save this search';
-
-  $scope.add = function(){
-      $scope.terms.push($scope.newterm);
-      $scope.parameters.push($scope.parameter);
+  
+  $('.dropdown-menu a').click(function(){
+    $('#parameter').text($(this).text());
+  });
+//bind pair and add to array
+   $scope.add = function() {
+    let parameter = $scope.parameter; 
+    let newTerm = $scope.newTerm;
+    let searchPair = {};
+    searchPair[parameter] = newTerm;
+    console.log("function is firing");
+    searchPairs.push(searchPair);
+    console.log(searchPairs);
   };
 //delete individual search terms:  
   $scope.del = function(i){
-    $scope.terms.splice(i,1);
-    $scope.parameters.splice(i,1);
+    $scope.searchPairs.splice(i,1);
   };
 //delete&reset entire search form:
   $scope.reset = function() {
-    $scope.terms.splice(0);
-    $scope.parameters.splice(0);
+    $scope.searchPairs.splice(0);
   };
 
 //saving your searches:
   $scope.save = function() {
-    alert ($scope.parameters, $scope.terms);
+    alert ($scope.searchPairs);
   };
     // $scope.save = function($index) {
     //   console.log($scope.terms);
